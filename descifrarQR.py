337977cd3f88285fca837c4cf5714bcd04e14d89
Ana López <anamuyamiga@gmail.com>
2024-02-06 12:13:22 +0100

    comiteoooo

--- descifrarQR.py
+++ descifrarQR.py
@@ -1,24 +1,33 @@
 import base64 as b64
+import mimetypes as mt
 
 texto = "Vm0wd2QyUXlWa2hWV0doVllteEtWMVl3WkRSWFJteFZVbTVrVmxKc2NIcFhhMk0xVmpGS2RHVkdXbFpOYm1oUVdWZDRTMk14VG5OWGJHUlRUVEZLVVZkV1kzaFRNVWw0V2toR1VtSlZXbFJXYWtwdlpWWmFkR05GU214U2JWSkpWbTEwYzJGV1NuUlZiR2hoVmpOb2FGWldXbUZqYkhCSlkwZDRVMkpXU2xsV1Z6QXhVekpHUjFOdVVtaFNlbXhXVm0weGIxSkdjRmRYYlhSWFRWWndlbFl5TVRSVk1rcFhVMnhzVjFaNlFYaFdSRXBIVWpGT2RWUnNhR2hsYlhoWlYxZDRiMVV3TUhoV1dHaFlZbGhTV0ZSV2FFTlRiR3QzV2tSU1ZrMUVSa1pXYlhCaFZqQXhkVlZ1V2xaaGExcGhXbFphVDJOdFNrZFRiV3hvWld4YWIxWnRNVEJXYXpGWFUydGtXRmRIYUZsWmEyaERZekZhYzFWclpGUmlSM2hYVmpKNGExWlhTa2RqUmxwWFlsaFNlbFpxUm1GU2JVVjZZVVprYUdFelFrbFdiWEJIVkRKU1YxZHVUbFJpVjNoVVZGY3hiMkl4V25STlJFWnJUVlZ3ZVZSV1ZtdGhiRXAwWVVoT1ZtRnJOVlJXTVZwaFkxWkdWVkpzVGs1WFJVcElWbXBLTkdFeFdsaFRiRnBxVWxkU1lWbFhjekZqYkZweFVtMUdVMkpWVmpaWlZWcHJWakZLVjJOSE9WaGhNVnBvVmtSS1RtVldUbkpoUjJoVFlrVndWVlp0TURGUk1rbDRWMWhvV0dKRk5WUlVWbFY0VGxaYWRFNVZPVmRpVlhCSVdUQmFjMWR0U2toaFJsSmFUVlp3VkZacVJuZFNWbEp5VGxkc1UySkhPVE5XYTFwaFZURlZlVkpyWkZoaWEzQndWV3RhZDFsV1duTlhibVJPVFZac00xWXlNVWRWTWtwR1RsUkdWazF1YUZoWlZWVjRZekZPY21KR2FGZFNXRUV5VjJ4V1lWbFdXWGhqUld4VllrWktjRlZxUmt0V1ZscEhWV3RLYTAxRVJsTlZSbEYzVUZFOVBRPT0="
 
 def descifrartexto(texto):
-  b64.b64decode(texto)
-  #b64.b64encode(input("Ingrese el texto a codificar: "))
+    texto_str = texto.encode('utf-8')
+    texto_2 = b64.b64decode(texto_str)
+    resultado = texto_2.decode('utf-8')
+    return resultado
    
-def desc_img(img):
+'''def desc_img(img):
     with open(img, 'rb') as image:
         image_read = image.read()
-        image_64_encode = b64.b64encode(image_read)
-        image_64_decode = b64.b64decode(image_64_encode)
-        with open('decoded_' + img, 'wb') as image_result:
-            image_result.write(image_64_decode)
-    return image_result
+    image_str = image_read.decode("utf-8")
+    image_64_encode = b64.b64encode(image_read)
+    image_str = image_64_encode.decode("utf-8")               #no se :(
+    mime = mt.MimeTypes()
+    imagen_mime = mime.guess_type(img)[0]
+    src = f'data:{imagen_mime};base64,',{image_str}
+    with open("imagen.txt", "w") as file:
+        file.write(str(src))
+    return src'''
 
 if __name__ == '__main__':
-  for i in range(8):
-    result_img = desc_img("flag.png")
-    print(result_img)
+    '''result_img = desc_img("flag.png")
+    print(result_img) # da un objeto random'''
     resultado_letra = descifrartexto(texto)
-    print(resultado_letra)
+    for i in range(10):
+        resultado_letra = descifrartexto(resultado_letra)
+        print("\n")
+        print(resultado_letra)
 
